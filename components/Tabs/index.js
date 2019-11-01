51fc3bd0ced5f262b2d4bd624cb7dd4fa095de3a
John Afolabi <johnafolabi.da@gmail.com>
2019-11-01 09:30:06 +0100

    Step 2: Create Tabs and append to DOM

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,26 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    const topics = document.querySelector(".topics");
+    response.data.topics.forEach(topic => {
+      topics.append(Tabs(topic));
+    });
+    console.log(response);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+function Tabs(name) {
+  const tab = document.createElement("div");
+
+  tab.textContent = `${name}`;
+
+  tab.classList.add("tab");
+
+  return tab;
+}
